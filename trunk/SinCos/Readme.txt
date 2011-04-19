d120019748502ad755b9a011be6e5694c502ce1c
Kromster80 <Kromster80@a88c7852-38bc-83e4-3a77-4b995faa4c64>
2011-04-19 09:32:40 +0000

    Added SinCos doodle, it's very basic yet

--- trunk/SinCos/Readme.txt
+++ trunk/SinCos/Readme.txt
@@ -1,13 +1,11 @@
-//Sketching
+//SinCos
 
-This is a simple OpenGL demo I wrote out of boredom.
+This is a simple OpenGL demo I wrote for a test.
 
-Left-click - Refresh
-Middle-click - Pause
-Right-Click - Invert background
+Left-click - Rotate
 
 If You have any questions, comments, bugreports or requests about this app or just want to send a postcard - feel free to contact me by e-mail.
 
-Created by Krom on 2011.04.02
+Created by Krom on 2011.04.19
 Contacts: kromster80@gmail.com
 Official site: krom.reveur.de
---
+++ trunk/SinCos/Unit_Render.pas
@@ -0,0 +1,128 @@
+unit Unit_Render;
+interface
+uses Classes, Controls, dglOpenGL, OpenGL, KromOGLUtils, KromUtils, Math, Windows;
+
+
+type
+  TRender = class
+  private
+    h_DC: HDC;
+    h_RC: HGLRC;
+    xOld,yOld:integer;
+    xRot,yRot:single;
+    fAreaX,fAreaY:integer;
+  public
+    constructor Create(RenderFrame:HWND; InX,InY:integer);
+    destructor Destroy; override;
+
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure MouseMove(Shift: TShiftState; X, Y: Integer);
+
+    procedure SetArea(InX,InY:integer);
+    procedure RenderResize;
+    procedure Render;
+  end;
+
+
+{const
+  LightPos:Array[0..3] of GLfloat = (-10,-30,-30,0);
+  LightSpec:Array[0..3] of GLfloat = (0.7,0.7,0.7,0);
+  LightDiff:Array[0..3] of GLfloat = (1,0,1,1);}
+
+
+var
+  fRender:TRender;
+
+
+implementation
+uses Unit_Objects;
+
+
+constructor TRender.Create(RenderFrame:HWND; InX,InY:integer);
+begin
+  Inherited Create;
+
+  SetRenderFrame(RenderFrame, h_DC, h_RC);
+  SetRenderDefaults;
+  SetArea(InX,InY);
+
+  yRot := 30;
+
+  glEnable(GL_DEPTH_TEST);
+  glDisable(GL_LIGHTING);
+
+  {glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glLightfv(GL_LIGHT0, GL_POSITION, @LightPos);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, @LightSpec);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, @LightDiff);}
+
+  SetupVSync(true);
+end;
+
+
+destructor TRender.Destroy;
+begin
+  wglMakeCurrent(0,0);
+  wglDeleteContext(h_RC);
+  Inherited;
+end;
+
+
+procedure TRender.MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  if Button <> mbLeft then exit;
+  xOld := X; yOld := Y;
+end;
+
+
+procedure TRender.MouseMove(Shift: TShiftState; X, Y: Integer);
+begin
+  if ssLeft in Shift then
+  begin
+    xRot := xRot - (X-xOld);
+    yRot := yRot + (Y-yOld);
+    xOld := X; yOld := Y;
+  end;
+end;
+
+
+procedure TRender.SetArea(InX,InY:integer);
+begin
+  fAreaX := max(InX,1);
+  fAreaY := max(InY,1);
+  RenderResize;
+end;
+
+
+procedure TRender.RenderResize;
+begin
+  glViewport(0, 0, fAreaX, fAreaY);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity;
+  //gluOrtho2D(0, fAreaX, fAreaY, 0);
+  gluPerspective(60, fAreaX/fAreaY, 0.01, 50.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity;
+end;
+
+
+procedure TRender.Render;
+begin
+  glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity;           
+
+  glTranslatef(0,0,-1.1);
+
+  glScalef(0.1,0.1,0.1);
+  glRotatef(yRot, 1, 0, 0);
+  glRotatef(-xRot, 0, 1, 0);
+
+  fObjectCollection.Render;
+
+  glFinish;
+  SwapBuffers(h_DC);
+end;
+
+
+end.
