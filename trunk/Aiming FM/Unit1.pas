66c01e3d6f18f3b4fb5b290ddb7333215a0838f4
kromster80 <kromster80@a88c7852-38bc-83e4-3a77-4b995faa4c64>
2011-10-19 19:08:10 +0000

    Converted Aiming application into FireMonkeyHD

---
+++ trunk/Aiming FM/Unit1.pas
@@ -0,0 +1,110 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Dialogs, Diagnostics, FMX.Types3D,
+  FMX.Layers3D, Fmx.Platform.Win, FMX.Objects;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure Image1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Single);
+    procedure Image1MouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Single);
+  private
+    procedure OnIdle(Sender: TObject; var Done: Boolean);
+  end;
+
+const
+      FPS_INTERVAL      =    300;
+
+var
+  Form1: TForm1;
+  ExeDir:string;
+
+  SW: TStopWatch;
+  OldTimeFPS,OldFrameTimes,FrameTime,FrameCount: Int64;
+
+
+implementation
+{$R *.fmx}
+uses Unit_Render, Unit_Aiming;
+
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  SW := TStopWatch.Create;
+  SW.Start;
+
+  Randomize;
+
+  ExeDir := ExtractFilePath(ParamStr(0));
+
+  fRender := TRender.Create(FmxHandleToHWND(Handle), ClientWidth, ClientHeight);
+  fAiming := TAiming.Create;
+
+  Application.OnIdle := OnIdle;
+
+  FormResize(Self);
+end;
+
+
+procedure TForm1.OnIdle(Sender: TObject; var Done: Boolean);
+begin //Counting FPS
+
+  FrameTime := (SW.GetTimeStamp div 10000 - OldTimeFPS) ;
+  OldTimeFPS := SW.GetTimeStamp div 10000;
+
+  inc(OldFrameTimes, FrameTime);
+  inc(FrameCount);
+
+  if OldFrameTimes >= FPS_INTERVAL then begin
+    Caption := Format('%f fps', [1000 / OldFrameTimes * FrameCount]);
+    OldFrameTimes := 0;
+    FrameCount := 0;
+  end;
+
+  fRender.Render;
+  Done := False;
+end;
+
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  fAiming.Free;
+  fRender.Free;
+end;
+
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  if fRender=nil then exit;
+  fRender.SetArea(ClientWidth, ClientHeight);
+end;
+
+
+procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Single);
+begin
+  fAiming.TargetPosition := PointF(X - ClientWidth div 2, Y - ClientHeight div 2);
+end;
+
+procedure TForm1.Image1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Single);
+begin
+  if ssLeft in Shift then
+    fAiming.TargetVector := PointF((X - ClientWidth div 2) - fAiming.TargetPosition.X,
+                                   (Y - ClientHeight div 2) - fAiming.TargetPosition.Y);
+end;
+
+{procedure TForm1.FormKeyPress(Sender: TObject; var Key: Char);
+begin
+  if Key = 'q' then fAiming.UpdatePerformance;
+end;}
+
+
+end.
---
+++ trunk/Aiming FM/Unit_Render.pas
@@ -0,0 +1,181 @@
+unit Unit_Render;
+interface
+uses Classes, dglOpenGL, OpenGL, KromOGLUtils, Math, Windows, SysUtils;
+
+type
+  TRender = class
+    private
+      h_DC: HDC;
+      h_RC: HGLRC;
+      fAreaX,fAreaY:integer;
+      csCircle:GLUint;
+      procedure CompileCommonShapes;
+    public
+      constructor Create(RenderFrame:HWND; InX,InY:integer);
+      destructor Destroy; override;
+      procedure SetArea(InX,InY:integer);
+      procedure RenderResize;
+      procedure Render;
+    end;
+
+    
+var
+  fRender:TRender;
+
+
+implementation
+uses Unit1, Unit_Aiming;
+
+
+constructor TRender.Create(RenderFrame:HWND; InX,InY:integer);
+begin
+  SetRenderFrame(RenderFrame, h_DC, h_RC);
+  SetRenderDefaults;
+  SetArea(InX,InY);
+
+  glDisable(GL_LIGHTING);
+  //glDisable(GL_BLEND);
+
+  glEnable(GL_LINE_SMOOTH);
+  glEnable(GL_POINT_SMOOTH);
+
+  glLineWidth(1);
+
+  BuildFont(h_DC, 12, FW_BOLD);
+  SetupVSync(true);
+
+  CompileCommonShapes;
+end;
+
+
+destructor TRender.Destroy;
+begin
+  wglMakeCurrent(0,0);
+  wglDeleteContext(h_RC);
+  Inherited;
+end;
+
+
+procedure TRender.CompileCommonShapes;
+const Sides = 100;
+var i:integer;
+begin
+  csCircle := glGenLists(1);
+  glNewList(csCircle, GL_COMPILE);
+  glBegin(GL_LINE_LOOP);
+    for i:=0 to Sides-1 do
+    glVertex2f(sin(i/Sides*2*pi), cos(i/Sides*2*pi));
+
+  glEnd;
+  glEndList;
+end;
+
+
+procedure TRender.SetArea(InX,InY:integer);
+begin
+  fAreaX := max(InX,1);
+  fAreaY := max(InY,1);
+  RenderResize;
+end;
+
+
+procedure TRender.RenderResize;
+begin
+  glViewport(0, 0, fAreaX, fAreaY);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity;
+  gluOrtho2D(0, fAreaX, fAreaY, 0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity;
+end;
+
+
+procedure TRender.Render;
+var i:integer;
+begin
+  glClearColor(1, 1, 1, 1);
+  glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity;
+
+  glPushMatrix;
+  glTranslatef(fAreaX/2, fAreaY/2, 0);
+
+  //Timestamps of flight distance
+  glPointSize(12);
+  for i:=1 to round(max(fAiming.GetTime,5)*5) do
+  begin
+    if i mod 5 = 0 then
+      glColor3f(0.5,0.5,0.5)
+    else
+      glColor3f(0.9,0.9,0.9);
+    glPushMatrix;
+      glScalef(fAiming.ArrowSpeed*i/5, fAiming.ArrowSpeed*i/5, fAiming.ArrowSpeed*i/5);
+      glCallList(csCircle);
+    glPopMatrix;
+  end;
+
+  //Start and Target
+  glColor3f(0,0,0);
+  glBegin(GL_POINTS);
+    glvertex2f(0,0);
+    glvertex2fv(@fAiming.TargetPosition);
+  glEnd;
+
+  //Target movement vector
+  glBegin(GL_LINE_STRIP);
+    glvertex2fv(@fAiming.TargetPosition);
+    glvertex2f(fAiming.GetTarget(1).X, fAiming.GetTarget(1).Y);
+    glColor3f(0.75, 0.75, 0.75);
+    glvertex2f(fAiming.GetTarget(max(fAiming.GetTime,5)).X, fAiming.GetTarget(max(fAiming.GetTime,5)).Y);
+  glEnd;
+
+  glPointSize(8);
+
+  //Timestamps of target movement
+  glColor3f(0.5,0.5,0.5);
+  glBegin(GL_POINTS);
+    for i:=1 to round(max(fAiming.GetTime,5)) do
+    glvertex2f(fAiming.GetTarget(i).X, fAiming.GetTarget(i).Y);
+  glEnd;
+
+  if fAiming.GetHit then
+  begin
+    glColor3f(1,0,0);
+    glPointSize(10);
+
+    //Target hit
+    glBegin(GL_POINTS);
+      glvertex2f(fAiming.GetTarget(fAiming.GetTime).X, fAiming.GetTarget(fAiming.GetTime).Y);
+    glEnd;
+
+    //Arrow flight vector
+    glBegin(GL_LINES);
+      glvertex2f(0,0);
+      glvertex2f(fAiming.GetTarget(fAiming.GetTime).X, fAiming.GetTarget(fAiming.GetTime).Y);
+    glEnd;
+  end;
+
+  glPopMatrix;
+
+  if fAiming.GetHit then
+  begin
+    glColor3f(1,0,0);
+    glRasterPos2f(10, fAreaY-60);
+    glPrint('Target will be hit in '+floattostr(RoundTo(fAiming.GetTime,-3))+' seconds');
+  end else
+  begin
+    glColor3f(0,0.6,0);
+    glRasterPos2f(10, fAreaY-60);
+    glPrint('Target will never be hit');
+  end;
+
+  glColor3f(0,0,0);
+  glRasterPos2f(10, fAreaY-20);
+  glPrint('Click and drag left mouse button to place an enemy and set its movement vector');
+
+  glFinish;
+  SwapBuffers(h_DC);
+end;
+   
+
+end.
