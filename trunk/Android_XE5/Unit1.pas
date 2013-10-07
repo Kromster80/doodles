3f12298a8a78d8db6c5fdc444754298ff699d685
kromster80 <kromster80@a88c7852-38bc-83e4-3a77-4b995faa4c64>
2013-10-07 19:48:32 +0000

    Working texture in xe5

--- trunk/Android_XE5/Unit1.pas
+++ trunk/Android_XE5/Unit1.pas
@@ -3,95 +3,275 @@ unit Unit1;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.IoUtils,
   FMX.Types, FMX.Controls, FMX.Forms3D, FMX.Forms, FMX.Graphics, FMX.Types3D
   {$IFDEF ANDROID}
-  ,Androidapi.Gles, Androidapi.Gles2
+  ,Androidapi.Gles2
+  ,Androidapi.Gles2ext
   {$ENDIF}
   {$IFDEF WIN32}
-  ,Winapi.OpenGL
+  ,Winapi.OpenGL, FMX.Controls3D, FMX.Objects3D
   {$ENDIF};
 
 type
   TVertice = record
-    X, Y, Z, R, G, B, A: Single;
+    X, Y, Z, U, V: Single;
   end;
 
   TForm1 = class(TForm3D)
     Timer1: TTimer;
     procedure Form3DRender(Sender: TObject; Context: TContext3D);
     procedure Form3DCreate(Sender: TObject);
-    procedure Form3DDestroy(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
   private
-    fPos: array of TVertice;
-    fInd: array of Integer;
+    fPos: array [0..2] of TVertice;
     fVtxShd: GLUint;
-    fIndShd: GLUint;
+    fText1: GLUint;
+    mProgramObject: Integer;
+    maPositionHandle: Integer;
+    gvTexCoordHandle: Integer;
+    gvSamplerHandle: Integer;
+    d: array [0..512*512] of Cardinal;
   end;
 
 var
   Form1: TForm1;
 
 implementation
-
 {$R *.fmx}
+uses
+  FMX.Consts;
+
+const
+  GLESHeaderHigh: array [0..24] of byte =
+    (Byte('p'), Byte('r'), Byte('e'), Byte('c'), Byte('i'), Byte('s'), Byte('i'), Byte('o'), Byte('n'), Byte(' '),
+     Byte('h'), Byte('i'), Byte('g'), Byte('h'), Byte('p'), Byte(' '), Byte(' '), Byte(' '), Byte('f'), Byte('l'),
+     Byte('o'), Byte('a'), Byte('t'), Byte(';'), Byte(#13));
+
+procedure RaiseContextExceptionFmt(ResStringRec: PResStringRec; const Args: array of const);
+begin
+  {$IFDEF ANDROID}
+  Log.d('[Context Exception]: ' + Format(LoadResString(ResStringRec), Args));
+  {$ENDIF}
+  EContext3DException.CreateResFmt(ResStringRec, Args);
+end;
+
+function GLGetErrorFlags: Integer;
+const
+  MaxStopLock = 16;
+var
+  Flag, StopLock: Integer;
+begin
+  Result := 0;
+
+  StopLock := MaxStopLock;
+
+  repeat
+    Flag := glGetError;
+    if Flag <> GL_NO_ERROR then
+      Result := Result or Flag;
+
+    Dec(StopLock);
+  until (Flag = GL_NO_ERROR) or (StopLock <= 0);
+end;
+
+function GLHasAnyErrors: Boolean;
+begin
+  Result := GLGetErrorFlags <> 0;
+end;
+
+function BuildShader(AType: Integer; const ACode: String): Integer;
+var
+  log: array of Byte;
+  len, compiled: Integer;
+  code: array of Byte;
+  I: Integer;
+begin
+  Result := 0;
+
+    if AType = GL_FRAGMENT_SHADER then
+    begin
+      SetLength(code, Length(GLESHeaderHigh) + Length(ACode));
+      for I := 0 to High(GLESHeaderHigh) do
+        code[I] := GLESHeaderHigh[I];
+      for I := 0 to High(ACode) do
+        code[Length(GLESHeaderHigh) + I] := Ord(ACode[I]);
+    end else begin
+      SetLength(code, Length(ACode));
+      for I := 0 to High(ACode) do
+        code[I] := Ord(ACode[I]);
+    end;
+    Result := glCreateShader(AType);
+    len := Length(code);
+    glShaderSource(Result, 1, @code, @len);
+    glCompileShader(Result);
+    glGetShaderiv(Result, GL_COMPILE_STATUS, @compiled);
+    if compiled = 0 then
+    begin
+      glGetShaderiv(Result, GL_INFO_LOG_LENGTH, @compiled);
+      if (compiled > 0) then
+      begin
+        {$WARNINGS OFF}
+        SetLength(log, compiled);
+        glGetShaderInfoLog(Result, compiled, @compiled, MarshaledAString(log));
+        FMX.Types.Log.d(MarshaledAString(log));
+        if AType = GL_VERTEX_SHADER then
+          RaiseContextExceptionFmt(@SCannotCreateVertexShader, [''])
+        else
+          RaiseContextExceptionFmt(@SCannotCreatePixelShader, ['']);
+        {$WARNINGS ON}
+      end;
+    end;
+    if (GLHasAnyErrors()) then
+      RaiseContextExceptionFmt(@SCannotCreateShader, ['']);
+end;
+
 
 procedure TForm1.Form3DCreate(Sender: TObject);
-begin
-  //glGenBuffers(1, @fVtxShd);
-  //glGenBuffers(1, @fIndShd);
+const
+  vShaderStr: String =
+            'attribute vec4 vPosition;    '
+          + 'attribute vec2 a_texCoord;   '
+          + 'varying vec2 v_texCoord;     '
+          + 'void main()                  '
+          + '{                            '
+          + '   gl_Position = vPosition;  '
+          + '   v_texCoord = a_texCoord;  '
+          + '}                            ';
 
-  SetLength(fPos, 3);
-  SetLength(fInd, 1);
+  fShaderStr: String =
+            'precision mediump float;                     '
+          + 'varying vec2 v_texCoord;                     '
+          + 'uniform sampler2D s_texture;                 '
+          + 'void main()                                  '
+          + '{                                            '
+          + '  gl_FragColor = texture2D(s_texture, v_texCoord); '
+          + '}                                            ';
+var
+  vertexShader: Integer;
+  fragmentShader: Integer;
+  programObject: Integer;
+  linked: Integer;
+  ms: TMemoryStream;
+begin
+        // Load the vertex/fragment shaders
+        vertexShader := BuildShader(GL_VERTEX_SHADER, vShaderStr);
+        fragmentShader := BuildShader(GL_FRAGMENT_SHADER, fShaderStr);
+
+        // Create the program object
+        programObject := glCreateProgram();
+
+        if (programObject = 0) then
+            Exit;
+
+        glAttachShader(programObject, vertexShader);
+        glAttachShader(programObject, fragmentShader);
+
+        // Bind vPosition to attribute 0
+        //glBindAttribLocation(programObject, 0, 'vPosition');
+
+        // Link the program
+        glLinkProgram(programObject);
+
+        // Check the link status
+        glGetProgramiv(programObject, GL_LINK_STATUS, @linked);
+
+        if (linked = 0) then
+        begin
+            Log.TimeStamp('Error linking program:');
+            //Log.TimeStamp(glGetProgramInfoLog(programObject));
+            glDeleteProgram(programObject);
+            Exit;
+        end;
+
+        maPositionHandle := glGetAttribLocation(programObject, 'vPosition');
+        gvTexCoordHandle := glGetAttribLocation(programObject, 'a_texCoord');
+        gvSamplerHandle := glGetUniformLocation(programObject, 's_texture');
+
+
+        // Store the program object
+        mProgramObject := programObject;
+
+        glClearColor(0.7, 0.7, 0.7, 1);
+
+
+  ms := TMemoryStream.Create;
+  try
+    ms.LoadFromFile(TPath.Combine(TPath.GetDocumentsPath, 'Tiles512.tga'));
+    ms.Position := 0;
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1, @fText1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, fText1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    //FillChar(d[0], SizeOf(d), $ff);
+    //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64, 0, GL_RGBA, GL_UNSIGNED_BYTE, @d[0]);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 512, 512, 0, GL_RGB, GL_UNSIGNED_BYTE, ms.Memory);
+  finally
+    ms.Free;
+  end;
+        Log.TimeStamp(IntToStr(glGetError));
+        Log.TimeStamp(IntToStr(glGetError));
+        Log.TimeStamp(IntToStr(glGetError));
+
+  Caption := Context.QualifiedClassName;
 end;
 
-procedure TForm1.Form3DDestroy(Sender: TObject);
-begin
-  //glDeleteBuffers(1, @fVtxShd);
-  //glDeleteBuffers(1, @fIndShd);
-end;
 
 procedure TForm1.Form3DRender(Sender: TObject; Context: TContext3D);
 begin
-  Context.BeginScene;
+  if Context.BeginScene then
+  try
+    //Log.TimeStamp(IntToStr(glGetError));
 
-  Log.TimeStamp(IntToStr(glGetError));
+    fPos[0].X := Random-0.5; fPos[0].Y := Random-0.5;  fPos[0].Z := 0;
+    fPos[1].X := Random-0.5; fPos[1].Y := Random-0.5;  fPos[1].Z := 0;
+    fPos[2].X := Random-0.5; fPos[2].Y := Random-0.5;  fPos[2].Z := 0;
 
-  glClearColor(Random, Random, Random, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
+    fPos[0].U := Random-0.5; fPos[0].V := Random-0.5;
+    fPos[1].U := Random-0.5; fPos[1].V := Random-0.5;
+    fPos[2].U := Random-0.5; fPos[2].V := Random-0.5;
 
-  fPos[0].X := Random(10); fPos[0].Y := Random(10);  fPos[0].Z := Random(10);
-  fPos[1].X := Random(10); fPos[1].Y := Random(10);  fPos[1].Z := Random(10);
-  fPos[2].X := Random(10); fPos[2].Y := Random(10);  fPos[2].Z := Random(10);
+        // Set the viewport
+        //glViewport(0, 0, Width, Height);
 
-  fInd[0] := 0;
-  fInd[1] := 1;
-  fInd[2] := 2;
+        // Clear the color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
 
-  Context.EndScene;
-  {glBindBuffer(GL_ARRAY_BUFFER, fVtxShd);
-  glBufferData(GL_ARRAY_BUFFER, Length(fPos) * SizeOf(TVertice), @fPos[0].X, GL_STREAM_DRAW);
+        // Use the program object
+        glUseProgram(mProgramObject);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, fIndShd);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, Length(fInd) * SizeOf(fInd[0]), @fInd[0], GL_STREAM_DRAW);
+        // Load the vertex data
+        glVertexAttribPointer(maPositionHandle, 3, GL_FLOAT, 0, 20, @fPos[0].X);
+        glVertexAttribPointer(gvTexCoordHandle, 2, GL_FLOAT, 0, 20, @fPos[0].U);
+        glEnableVertexAttribArray(maPositionHandle);
+        glEnableVertexAttribArray(gvSamplerHandle);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, fText1);
+        glUniform1i(gvSamplerHandle, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(maPositionHandle);
+        glDisableVertexAttribArray(gvTexCoordHandle);
 
-  //Setup vertex and UV layout and offsets
-  glEnableClientState(GL_VERTEX_ARRAY);
-  glEnableClientState(GL_COLOR_ARRAY);
-  glVertexPointer(3, GL_FLOAT, SizeOf(TVertice), Pointer(0));
-  glColorPointer(4, GL_FLOAT, SizeOf(TVertice), Pointer(12));
-
-  //Here and above OGL requests Pointer, but in fact it's just a number (offset within Array)
-  glDrawElements(GL_TRIANGLES, Length(fInd), GL_UNSIGNED_INT, Pointer(0));
-
-  glDisableClientState(GL_VERTEX_ARRAY);
-  glDisableClientState(GL_COLOR_ARRAY);}
+{        glVertexAttribPointer(maPositionHandle, 3, GL_FLOAT, 0, 20, @fPos[0].X);
+        glEnableVertexAttribArray(maPositionHandle);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(maPositionHandle);}
+  finally
+    Context.EndScene;
+  end;
 end;
+
 
 procedure TForm1.Timer1Timer(Sender: TObject);
 begin
   InvalidateRect(RectF(0, 0, width, height));
 end;
+
 
 end.
