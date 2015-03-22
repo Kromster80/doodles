762cbcebc056b6637be6d3e0674c30f3fd3a9326
kromster80 <kromster80@gmail.com>
2015-03-22 10:47:15 +0300

    Updated clean.bat

--- clean.bat
+++ clean.bat
@@ -1,6 +1,11 @@
+;Delete folders recursively
+FOR /D /R %%X IN (__history) DO RD /S /Q "%%X"
+FOR /D /R %%X IN (backup) DO RD /S /Q "%%X"
+
 erase /F /Q /S *.~* *.ddp *.drc *.dcp *.dcu
-erase /F /Q /S *.o *.or *.ppu *.compiled
-erase /F /Q /S *.log *.tmp thumbs*.db *.ion *.skincfg
+erase /F /Q /S *.o *.or *.ppu *.compiled *.local
+erase /F /Q /S *.tmp *.log thumbs.db descript.ion *.skincfg *.identcache *.tvsconfig *.stat
+
 erase /F /Q /S /A:H *.~* *.ddp *.drc *.dcp *.dcu
-erase /F /Q /S /A:H *.o *.or *.ppu *.compiled
-erase /F /Q /S /A:H *.log *.tmp thumbs.db *.ion *.skincfg
+erase /F /Q /S /A:H *.o *.or *.ppu *.compiled *.local
+erase /F /Q /S /A:H *.tmp *.log thumbs.db descript.ion *.skincfg *.identcache *.tvsconfig *.stat
